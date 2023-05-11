8825df86a1f02c5b1f59549c58caefc98d325548
JohnEverton-Dev <j.everton.ti@gmail.com>
2023-05-11 17:09:43 -0300

    Configuração do projeto

---
+++ src/main/java/com/alvestech/oficinadainformatica/OficinaDaInformaticaApplication.java
@@ -0,0 +1,13 @@
+package com.alvestech.oficinadainformatica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OficinaDaInformaticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OficinaDaInformaticaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alvestech/oficinadainformatica/OficinaDaInformaticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvestech.oficinadainformatica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OficinaDaInformaticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
